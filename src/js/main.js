674c8d4619d41923f7719270a299976c1690318c
= <=>
2018-04-15 04:39:57 +0600

    Закончен слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$(".examples__slider").slick({
+		dots: true,
 		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
     nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
 	});
