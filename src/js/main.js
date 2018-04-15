8f7e2aeea6c85409b734248c7f107b4ebb0a08bc
fregire <fregire@mail.ru>
2018-04-15 14:06:32 +0500

    Написан срипт для вставки url видео

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
+	// Активация слайдеров
+
+	// Актвивация слайдера работ
 	$(".examples__slider").slick({
 		dots: true,
 		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
     nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
 	});
-	// TODO: Активировать слайдер slick + сделать секцию для миниатюры, 
-	// если видео, менять путь, если фрейм - ссылку + стилизовать четыре элемента на превью
+
+	// Активация слайдера трансляций
+	$(".video-slider__slides").slick({
+		dots: true,
+		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
+    nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
+	});
+
+	// Подставляет в главное превью ссылку из ролика на ютубе
+	$(".video-slider__item").on("click", function() {
+		var url = $(this).children(".video-slider__preview").attr("data-url");
+		var urlString = url.split("watch?v=");
+		if(urlString.length === 1) {
+			$(".video-slider__player").attr("src", urlString);
+		} else {
+			$(".video-slider__player").attr("src", urlString.join("embed/"));
+		}
+		
+	});
 });
