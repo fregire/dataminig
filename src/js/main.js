0cb78c661497ea0535ed161825725ec4db1a8d77
= <=>
2018-04-15 13:17:34 +0600

    Текст над видео-слайдером

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,6 @@ $(document).ready(function() {
 		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
     nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
 	});
+	// TODO: Активировать слайдер slick + сделать секцию для миниатюры, 
+	// если видео, менять путь, если фрейм - ссылку + стилизовать четыре элемента на превью
 });
