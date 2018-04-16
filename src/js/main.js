8ce37642fbc712be5ca6f85d0910e336020c0b2f
fregire <fregire@mail.ru>
2018-04-16 20:57:49 +0500

    Готоа один элемент - продукт

--- src/js/main.js
+++ src/js/main.js
@@ -5,28 +5,29 @@ $(document).ready(function() {
 	$(".examples__slider").slick({
 		dots: true,
 		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
-    nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>',
-    responsive: [
-    	{
-    		breakpoint: 760,
-    		settings: {
-    			slidesToShow: 1
-    		}
-    	},
-    	{
-    		breakpoint: 1000,
-    		settings: {
-    			slidesToShow: 2,
-    			centerMode: true
-    		}
-    	},
-    	{
-    		breakpoint: 1200,
-    		settings: {
-    			slidesToShow: 3
-    		}
-    	}
-    ]
+    	nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>',
+    	responsive: [
+	    	{
+	    		breakpoint: 1000,
+	    		settings: {
+	    			slidesToShow: 2,
+	    			centerMode: true,
+	    			vertical: false
+	    		}
+	    	},
+	    	{
+	    		breakpoint: 760,
+	    		settings: {
+	    			slidesToShow: 1,
+	    			centerMode: true,
+	    			vertical: false
+	    		}
+	    	}
+
+    	],
+    	slidesToShow: 3,
+    	vertical: true,
+    	centerMode: false
 	});
 
 	// Активация слайдера трансляций
