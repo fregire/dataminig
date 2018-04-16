7d0be0f39a576305f687ff74989767b621cb1921
= <=>
2018-04-16 17:41:30 +0600

    Для разрешений 480px

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,22 @@ $(document).ready(function() {
 	$(".examples__slider").slick({
 		dots: true,
 		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
-    nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
+    nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>',
+    responsive: [
+    	{
+    		breakpoint: 760,
+    		settings: {
+    			slidesToShow: 1
+    		}
+    	},
+    	{
+    		breakpoint: 900,
+    		settings: {
+    			slidesToShow: 2,
+    			centerMode: true
+    		}
+    	}
+    ]
 	});
 
 	// Активация слайдера трансляций
