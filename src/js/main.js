11213c4a1ce4ec963482e36c27e2b2dab084bd91
= <=>
2018-04-17 23:58:34 +0600

    Фото галлерея

--- src/js/main.js
+++ src/js/main.js
@@ -47,16 +47,11 @@ $(document).ready(function() {
 		prevArrow: '<button class="slick-prev"><i class="arrow arrow--prev" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Назад</span></button>',
     nextArrow: '<button class="slick-next"><i class="arrow arrow--next" arial-hidden="true"></i><span arial-hidden="true" class="visually-hidden">Вперед</span></button>'
 	});
-
-	// Подставляет в главное превью ссылку из ролика на ютубе
-	$(".video-slider__item").on("click", function() {
-		var url = $(this).children(".video-slider__preview").attr("data-url");
-		var urlString = url.split("watch?v=");
-		if(urlString.length === 1) {
-			$(".video-slider__player").attr("src", urlString);
-		} else {
-			$(".video-slider__player").attr("src", urlString.join("embed/"));
-		}
-		
+	// Галлерея
+	$('.video__link').magnificPopup({
+	  type: 'image',
+	  gallery: {
+	      enabled:true
+	  }
 	});
 });
